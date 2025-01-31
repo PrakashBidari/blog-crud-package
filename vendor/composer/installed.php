7e6a9484c409cc0d4daa8cca3782c73aff716b96
PrakashBidariS <prakashbidari.sixsigma@gmail.com>
2025-01-31 11:09:19 +0545

    stable-version-solve

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'crudpackage/blogcrud',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '37b178529cb1482bca6165bc02a8706bb67d94ea',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'crudpackage/blogcrud' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '37b178529cb1482bca6165bc02a8706bb67d94ea',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
